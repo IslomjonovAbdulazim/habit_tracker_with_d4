1893fcf258951d8d46b7c0b52800c20d176907e1
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-12 17:23:12 +0500

    imp

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,3 +1,17 @@
 import 'package:get/get.dart';
+import 'package:habit_tracker_with_d4/models/habit_model.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  RxList<HabitModel> habits = <HabitModel>[].obs;
+
+  @override
+  void onInit() {
+    load();
+    super.onInit();
+  }
+
+  void load() async {
+    habits.value = await habitReadAll();
+  }
+}
+
